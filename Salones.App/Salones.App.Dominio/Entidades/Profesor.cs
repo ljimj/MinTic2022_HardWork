f0e55d98c6cfdccc4517faea4993914b1e7bfb70
Marco Antonio Achury <67766219+marcoachury@users.noreply.github.com>
2021-09-14 18:45:23 -0500

    profe

--- Salones.App/Salones.App.Dominio/Entidades/Profesor.cs
+++ Salones.App/Salones.App.Dominio/Entidades/Profesor.cs
@@ -2,6 +2,8 @@ using System;
 
 namespace Salones.App.Dominio
 {
+    // Clase para representar profesores
+    // Hereda de la clase Persona
     public class Profesor:Persona
     {
         public string departamento{get;set;}
