1bc115cdf8a2cb5b16c1f4771a4b0496b8f97af4
cristhianLeon <cristhiancleon@gmail.com>
2021-09-20 08:52:44 -0500

    Interfaz CRUD profesor

---
+++ Salones.App/Salones.App.Persistencia/Migrations/20210920074138_Segunda.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Salones.App.Persistencia.Migrations
+{
+    public partial class Segunda : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "identificación",
+                table: "Personas",
+                newName: "identificacion");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "identificacion",
+                table: "Personas",
+                newName: "identificación");
+        }
+    }
+}
