9f212482ca5095ba6968bec5b6e15bb0701e3501
cristhianLeon <cristhiancleon@gmail.com>
2021-09-22 01:39:48 -0500

    dudas covid

--- Salones.App/Salones.App.Dominio/Entidades/Covid.cs
+++ Salones.App/Salones.App.Dominio/Entidades/Covid.cs
@@ -4,14 +4,16 @@ using System;
 namespace Salones.App.Dominio
 {
     //Estado COVID de una persona
+
+    // hereda de persona o persona hereda de covid
     public class Covid
     {         
         public int id {get;set;}
         
         public string sintomas{get;set;}
 
-        public EstadoCovid estadoCovid {get;set;}
-
+        public EstadoCovid estadoCovid {get;set;} //este no seria bool
+        
         public string fechaDiagnostico{get;set;}
 
         public string periodoAislamiento {get;set;}
