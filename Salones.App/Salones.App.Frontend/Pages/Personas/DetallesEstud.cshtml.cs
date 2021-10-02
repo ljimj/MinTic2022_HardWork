a6c17ac9eb0b2acba57e0e37eddb09ff17be9f07
Leonardo Jiménez <ljim230@gmail.com>
2021-10-02 15:40:10 -0500

    Detalles Estudiante

--- Salones.App/Salones.App.Frontend/Pages/Personas/DetallesEstud.cshtml.cs
+++ Salones.App/Salones.App.Frontend/Pages/Personas/DetallesEstud.cshtml.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Salones.App.Persistencia;
+using Salones.App.Dominio;
 
 namespace Salones.App.Frontend.Pages
 {
     public class DetallesEstudModel : PageModel
     {
-        public void OnGet()
+        private static IRepositorioEstudiante _repoEstudiante = new RepositorioEstudiante(new Persistencia.AppContext());
+        
+        public Estudiante estudiante{get;set;}
+        
+        public IActionResult OnGet(int estudId)
         {
+            estudiante = _repoEstudiante.GetEstudiante(estudId);
+            if(estudiante == null)
+            {
+                return RedirectToPage("/Index");
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
--- Salones.App/Salones.App.Frontend/Pages/Personas/ListEstud.cshtml.cs
+++ Salones.App/Salones.App.Frontend/Pages/Personas/ListEstud.cshtml.cs
@@ -4,13 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Salones.App.Persistencia;
+using Salones.App.Dominio;
 
 namespace Salones.App.Frontend.Pages
 {
     public class ListEstudModel : PageModel
     {
+        private static IRepositorioEstudiante _repoEstudiante = new RepositorioEstudiante(new Persistencia.AppContext());
+        
+        public IEnumerable<Estudiante> estudiantes{get;set;}
+        
         public void OnGet()
         {
+            estudiantes = _repoEstudiante.GetAllEstudiantes();
         }
     }
 }
