84a11f22a8a69f265dc47dc506d58ad1ef7dcf03
Marco Achury <marcoachury@gmail.com>
2021-10-14 17:20:24 -0500

    Instalador de paquetes.  Ejecutar este despues de instalar punto net core 5.0

---
+++ Salones.App/Salones.App.Frontend/paquetes.bat
@@ -0,0 +1,10 @@
+dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design --version 5.0.2
+
+dotnet add package Microsoft.EntityFrameworkCore.Design --version 5.0.9
+
+dotnet add package Microsoft.AspNetCore.Identity.EntityFrameworkCore --version 5.0.9
+
+dotnet add package Microsoft.AspNetCore.Identity.UI --version 5.0.9
+
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 5.0.9
+
