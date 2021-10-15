b4fc6513a42f138f885c6d1462144427ef36343a
Marco Achury <marcoachury@gmail.com>
2021-10-14 19:13:27 -0500

    Importadas librerias para validar usuarios

--- Salones.App/Salones.App.Frontend/paquetes.bat
+++ Salones.App/Salones.App.Frontend/paquetes.bat
@@ -1,3 +1,19 @@
+@echo off
+echo.
+echo INSTALADOR DE PAQUETES  -  DOT NET CORE 5.0
+echo.
+echo Este programa instala automaticamente los paquetes necesarios.
+echo Solo debería ejecutarse una vez.
+echo.
+echo No funciona bien en la consola Powershell del VSCode, debería 
+echo ejecutarse desde la consola de windows.
+echo
+echo on
+
+dotnet tool install -g dotnet-aspnet-codegenerator --version="3.1" 
+
+dotnet tool update --global dotnet-aspnet-codegenerator
+
 dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design --version 5.0.2
 
 dotnet add package Microsoft.EntityFrameworkCore.Design --version 5.0.9
@@ -7,4 +23,3 @@ dotnet add package Microsoft.AspNetCore.Identity.EntityFrameworkCore --version 5
 dotnet add package Microsoft.AspNetCore.Identity.UI --version 5.0.9
 
 dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 5.0.9
-
